bfb82a6e76ce3802fb93c49212e8af290b543eff
aaglis <aglissilvafon@gmail.com>
2024-04-29 23:56:37 -0300

    refactor: add default max-width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { RedirectButtonComponent } from './shared/components/redirect-button/redirect-button.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, InicioComponent, RedirectButtonComponent],
+  imports: [RouterOutlet, HeaderComponent, InicioComponent, RedirectButtonComponent, SobreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/sobre/components/highlight-panel/highlight-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlight-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './highlight-panel.component.html',
+  styleUrl: './highlight-panel.component.scss'
+})
+export class HighlightPanelComponent {
+
+}
