9e87ae97781ab02b5c2c3b2aab0d97d5a04d3930
aaglis <aglissilvafon@gmail.com>
2024-06-01 00:22:12 -0300

    refactor: code adjusts

--- src/app/components/sobre/components/highlight-panel/highlight-panel.component.ts
+++ src/app/components/sobre/components/highlight-panel/highlight-panel.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-highlight-panel',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './highlight-panel.component.html',
   styleUrl: './highlight-panel.component.scss'
 })
 export class HighlightPanelComponent {
-
+  @Input() imageUrl: string = ''
+  @Input() preTitle: string = ''
+  @Input() title: string = ''
+  @Input() subtext: string = ''
+  @Input() imagePosition: string = ''
 }
