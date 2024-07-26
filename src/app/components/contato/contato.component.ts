37b89d8d341f51cbae80cee3f4ec33d110cd2a9a
aaglis <aglissilvafon@gmail.com>
2024-07-26 15:57:52 -0300

    feat: section contato width sweetalert and a simple spam control

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { LucideAngularModule } from 'lucide-angular';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { ContatoService } from './services/contato.service';
+import { LucideAngularModule } from 'lucide-angular';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contato',
@@ -15,17 +16,47 @@ export class ContatoComponent {
 
   private contatoService = inject(ContatoService)
 
+  @ViewChild('buttonSubmit') buttonSubmit?: ElementRef;
+
   onSubmit(event: Event) {
     event.preventDefault()
-    const email = (document.querySelector('#email') as HTMLInputElement).value;
-    const message = (document.querySelector('#message') as HTMLTextAreaElement).value;
+    this.buttonSubmit?.nativeElement.setAttribute('disabled', 'true');
+
+    let email = (document.querySelector('#email') as HTMLInputElement).value;
+    let message = (document.querySelector('#message') as HTMLTextAreaElement).value;
 
     this.contatoService.sendEmail(email, message).then(() => {
       this.successMessage = 'Mensagem enviada com sucesso!';
       console.log(this.successMessage)
+      Swal.fire({
+        position: "top-end",
+
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        showCloseButton: true,
+        timer: 3000,
+        timerProgressBar: true,
+      });
+      email = ''
+      message = ''
+
+      setTimeout(() => {
+        this.buttonSubmit?.nativeElement.removeAttribute('disabled');
+      }, 100000)
     }).catch(() => {
       this.errorMessage = 'Houve um erro ao enviar a mensagem.';
       console.log(this.errorMessage)
+      Swal.fire({
+        position: "top-end",
+
+        icon: "error",
+        title: "Erro ao enviar a mensagem. Tente novamente.",
+        showConfirmButton: true,
+        showCloseButton: true,
+        timer: 3000,
+        timerProgressBar: true,
+      });
     });
   }
 }
